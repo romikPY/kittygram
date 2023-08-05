195e64091d57eaf82384a1471812e108a6cea561
Роман Козлов <roman.d.kozlov@yandex.ru>
2023-08-05 06:42:43 +0300

    lesson changes

--- cats/views.py
+++ cats/views.py
@@ -1,19 +1,65 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import generics
+from rest_framework import viewsets 
 
 from .models import Cat
 from .serializers import CatSerializer
 
+# viewsets
+class CatViewSet(viewsets.ModelViewSet):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer 
 
-@api_view(['GET', 'POST'])
-def cat_list(request):
-    if request.method == 'POST':
-        serializer = CatSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    cats = Cat.objects.all()
-    serializer = CatSerializer(cats, many=True)
-    return Response(serializer.data)
+# generics
+# class CatList(generics.ListCreateAPIView):
+#     queryset = Cat.objects.all()
+#     serializer_class = CatSerializer
+
+
+# class CatDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Cat.objects.all()
+#     serializer_class = CatSerializer 
+
+# View-функция cat_list() будет обрабатывать только запросы GET и POST, 
+# запросы других типов будут отклонены,
+# так что в теле функции их можно не обрабатывать
+# Api-функция
+# @api_view(['GET', 'POST'])
+# def cat_list(request):
+#     # Обработчик для POST-запросов.
+#     if request.method == 'POST':
+#         # Создаём объект сериализатора 
+#         # и передаём в него данные из POST-запроса
+#         serializer = CatSerializer(data=request.data)
+#         if serializer.is_valid():
+#             # Если полученные данные валидны —
+#             # сохраняем данные в базу через save().
+#             serializer.save()
+#             # Возвращаем JSON со всеми данными нового объекта
+#             # и статус-код 201
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#         # Если данные не прошли валидацию — 
+#         # возвращаем информацию об ошибках и соответствующий статус-код:
+#     # Получаем все объекты модели
+#     cats = Cat.objects.all()
+#     # Передаём queryset в конструктор сериализатора
+#     serializer = CatSerializer(cats, many=True)
+#     return Response(serializer.data)
+
+# API class
+# class APICat(APIView):
+#     def get(self, request):
+#         cats = Cat.objects.all()
+#         serializer = CatSerializer(cats, many=True)
+#         return Response(serializer.data)
+
+#     def post(self, request):
+#         serializer = CatSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED) 
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,9 +1,35 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import SimpleRouter
 
-from cats.views import cat_list
+# from cats.views import cat_list
+# from cats.views import APICat
+# from cats.views import CatList, CatDetail
+from cats.views import CatViewSet
+
+# Создаётся роутер
+router = SimpleRouter()
+# Вызываем метод .register с нужными параметрами
+router.register('cats', CatViewSet)
+# В роутере можно зарегистрировать любое количество пар "URL, viewset":
+# например
+# router.register('owners', OwnerViewSet)
+# Но нам это пока не нужно
 
 urlpatterns = [
-   path('cats/', cat_list),
+    # Все зарегистрированные в router пути доступны в router.urls
+    # Включим их в головной urls.py
+    path('', include(router.urls)),
 ]
 
+# urlpatterns = [
+#     path('cats/', CatList.as_view()),
+#     path('cats/<int:pk>/', CatDetail.as_view()),
+# ] 
 
+# urlpatterns = [
+#    path('cats/', cat_list),
+# ]
+
+# urlpatterns = [
+#     path('cats/', APICat.as_view()),
+# ]
